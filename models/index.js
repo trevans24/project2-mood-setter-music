bbe60c4c66a42566589fcb45887aa5c75ce16385
trevans24 <trevans24@gmail.com>
2017-04-20 21:00:42 -0600

    addid processes for heroku

--- models/index.js
+++ models/index.js
@@ -1,5 +1,8 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/mood-setter-music'); 
+mongoose.connect(process.env.MONGODB_URI || 
+                process.env.MONGOLAB_URI || 
+                process.env.MONGOHQ_URL ||
+                'mongodb://localhost/mood-setter-music'); 
 
 module.exports.Playlist = require('./playlist.js');
 module.exports.Songs = require('./songs.js');
