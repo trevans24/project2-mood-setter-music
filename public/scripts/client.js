7dbfdbaa73205e8ced8fec218e7c6d6c0e8b49de
trevans24 <trevans24@gmail.com>
2017-04-21 13:24:33 -0600

    added a form for adding playlist

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -8,11 +8,13 @@ $.get('/api/playlists', function(res){
 	// console.log(res);
 	res.forEach(function(playlist){
 		// console.log(playlist);
+		// console.log('test2');
 		renderPlaylist(playlist);
 	});
 });
 
-//test initializing
+
+//test initializing for search functionality
 SC.initialize({
   		client_id: '7PzyA3QRoqAdj9Veay4qRSuIKpYBghIf'
 		});
@@ -21,32 +23,31 @@ SC.initialize({
 SC.get('/playlists', {
 	kind: 'playlist',
 	sharing: 'public',
-	permalink: 'date'
-}).then(function(playlist){
-	// console.log(playlists[15]);
-	console.log(playlist[15].permalink_url);
+	title: 't'//this is the changable field
+}).then(function(playlistSearch){
+	console.log(playlistSearch[15]);
+	// console.log('test3');
+	console.log(playlistSearch[15].permalink_url);
+});
+// console.log('test1');
+
+
 });
 
-
-
-
-});
+// console.log('test');
 //function buids a single playlist button to render
 //this is for each new bubble
 function renderPlaylist(playlist){
+	// console.log('test4');
 	// console.log('Rendering: ', playlist);
 	// console.log(playlist._id);
 	var playlistHTML = 
 	"<div class='bubble' data-playlist-id='" + playlist._id + "' data-playlist-url='" + playlist.playlistURL + "'>" +
-	// "	<div class=''>" +
-	// "	<!-- internal section of playlist bubbles -->" +
 	"		<div class='playlist-body'>" +
 	"			<div class='col-md-1'>" +
 	"				<h4 class='playlist-name'>" + playlist.playlistName + "</h4>" +
 	"			</div>" +
 	"		</div> " +
-	// "		<!-- end of internal section -->" +
-	// "	</div>" +
 	"</div>";
 
 	$('#playlists').append(playlistHTML);
@@ -56,8 +57,8 @@ function renderPlaylist(playlist){
 		e.preventDefault();
 		var url = $(this).data("playlist-url");
 		// console.log(url);
+		// console.log('test5');
 		SC.Widget('test').load(url);
 	});
-
 
 }
